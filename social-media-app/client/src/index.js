33f90200d5d70dd4cac9774b93493b2c3beefb4b
charvi03 <charvikhurana01@gmail.com>
2024-08-15 19:09:23 +0530

    init

--- social-media-app/client/src/index.js
+++ social-media-app/client/src/index.js
@@ -10,15 +10,11 @@ import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- social-media-app/server/index.js
+++ social-media-app/server/index.js
@@ -9,8 +9,6 @@ import AuthRoute from "./routes/AuthRoute.js";
 import UserRoute from "./routes/UserRoute.js";
 import PostRoute from "./routes/PostRoute.js";
 import UploadRoute from "./routes/UploadRoute.js";
-import ChatRoute from "./routes/ChatRoute.js";
-import MessageRoute from "./routes/MessageRoute.js";
 
 const app = express();
 
@@ -35,5 +33,3 @@ app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
 app.use("/posts", PostRoute);
 app.use("/upload", UploadRoute);
-app.use("/chat", ChatRoute);
-app.use("/message", MessageRoute);
